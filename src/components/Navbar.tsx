b2266524b880f496c51fd2e7d03fec76ee607517
gelo <gelodev83@gmail.com>
2025-04-09 18:14:16 +0800

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     
 
   return (
-    <header className='py-[50px] px-[90px] absolute w-full'>
+    <header className='py-[50px] px-[83px] absolute w-full'>
         <nav className='flex items-center justify-between'> 
             <h1 className='font-[700] text-[40px] logo-gradient'>Bitcann</h1>
             <ul className='flex items-center text-white font-[400] text-[18px] gap-[6rem]'>
@@ -14,7 +14,7 @@ export default function Navbar() {
                 <li>
                     <Link href="/">Profile</Link>
                 </li>
-                <button className='bg-[#27A1FA] rounded-full py-[0.8rem] px-[2.2rem] cursor-pointer'>Connect Wallet</button>
+                <button className='bg-[#27A1FA] rounded-full py-[0.8rem] px-[2.2rem] cursor-pointer  duration-100 ease-out hover:bg-[#24c5ff]'>Connect Wallet</button>
             </ul>
         </nav>
     </header>
